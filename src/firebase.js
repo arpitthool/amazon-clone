12bfe7b0b62e4809b813a7f85e2e907971ad8bd8
arpitthool <arpitthool1@gmail.com>
2022-07-01 23:34:39 -0400

    firebase.js added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD80cEhSggfTDLF9GT51VYhKrwfS5Apf94",
+    authDomain: "challenge-3742b.firebaseapp.com",
+    projectId: "challenge-3742b",
+    storageBucket: "challenge-3742b.appspot.com",
+    messagingSenderId: "998508387464",
+    appId: "1:998508387464:web:dd7f54256582c3dfcb533d",
+    measurementId: "G-FXFGYWWXP8"
+  };
