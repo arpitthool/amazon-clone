05a418920fe253f384b4151d81f6e67772ff3420
arpitthool <arpitthool1@gmail.com>
2022-07-02 00:55:19 -0400

    Styled the header in header.css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     // BEM convention
     <div className="app">
-      <h1>Hello Clever Programmers, let's build the Amazon Store!</h1>
       <Header />
       {/* Home */}
     </div>
