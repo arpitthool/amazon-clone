ce2b5d2a0029bccf8baa9761a0e2c88ab84b8b7c
arpitthool <arpitthool1@gmail.com>
2022-07-02 19:46:45 -0400

    Added the checkout route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     // BEM convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+
+      <div className="app">
+
+        <Routes>
+
+          <Route exact path='/' element={<><Header/><Home/></>}/>
+          <Route exact path='/checkout' element={<><Header/><p>Basket</p></>} />
+
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
