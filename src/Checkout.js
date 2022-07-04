e79e226693c0c8e7fd8a9432c07bfc260758b5bb
arpitthool <arpitthool1@gmail.com>
2022-07-03 23:23:22 -0400

    displaying total items in basket along with basket total

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,23 +3,24 @@ import './Checkout.css'
 import Subtotal from './Subtotal'
 
 function Checkout() {
-  return (
-    <div className='checkout'>
-        <div className='checkout__left'>
-            <img className='checkout__ad' src='https://m.media-amazon.com/images/I/71zXsuikWhL._SX3000_.jpg'/>
-        
-            <div>
-                <h2 className='checkout__title'>
-                    Your shopping basket!
-                </h2>
+
+    return (
+        <div className='checkout'>
+            <div className='checkout__left'>
+                <img className='checkout__ad' src='https://m.media-amazon.com/images/I/71zXsuikWhL._SX3000_.jpg'/>
+            
+                <div>
+                    <h2 className='checkout__title'>
+                        Your shopping basket!
+                    </h2>
+                </div>
+            </div>
+
+            <div className='checkout__left'>
+                <Subtotal/>
             </div>
         </div>
-
-        <div className='checkout__left'>
-            <Subtotal/>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Checkout
