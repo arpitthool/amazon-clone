45de43eae7dbc583171eb49e775bc8f8551861a2
arpitthool <arpitthool1@gmail.com>
2022-07-04 17:06:08 -0400

    downgraded react-router version

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
-import { BrowserRouter as Router, Routes, Route, Switch} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Login from './Login';
 
 function App() {
   return (
     // BEM convention
     <Router>
+    <div className="app">
+      <Switch>
+        
+        <Route exact path="/">
+          <Header />
+          <Home />
+        </Route>
 
-      <div className="app">
-        <Routes>
+        <Route exact path="/checkout">
+          <Header />
+          <Checkout/>
+        </Route>
 
-          <Route exact path='/' element={<><Header/><Home/></>}/>
-          <Route exact path='/login' element={<><Login/></>}/>
-          <Route exact path='/checkout' element={<><Header/><Checkout/></>} />
-
-        </Routes>
-      </div>
-    </Router>
-    
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
