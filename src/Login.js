03c2a38cad1065cc37e6fed33adcc4710dea941a
arpitthool <arpitthool1@gmail.com>
2022-07-04 02:00:12 -0400

    created signIn() and register() which are called on respective button clicks

--- src/Login.js
+++ src/Login.js
@@ -1,45 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
-  return (
-    <div className='login'>
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        // to prevent from refreshing
+        e.preventDefault()
+
+        // TODO : firebase login
+    }
+
+    const register = e => {
+        // to prevent from refreshing
+        e.preventDefault()
+
+        // TODO : firebase register
+    }
+
+    return (
+        <div className='login'>
+                        
+            <Link to='/'>
+                <img
+                    className='login__logo' 
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'/>
+
+            </Link>
+
+            <div className='login__container'>
+                <h1>Sign In</h1>
+
+                <form>
+                    <h5>Email</h5>
+                    <input type='text' value={email} onChange ={
+                        e => setEmail(e.target.value)
+                    }/>
                     
-        <Link to='/'>
-            <img
-                className='login__logo' 
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'/>
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange ={
+                        e => setPassword(e.target.value)
+                    }/>
 
-        </Link>
+                    <button  type='submit' className='login__signInButton' onClick={signIn}>
+                        Sign In
+                    </button>
+                </form>
 
-        <div className='login__container'>
-            <h1>Sign In</h1>
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
 
-            <form>
-                <h5>Email</h5>
-                <input type='text'/>
-                
-                <h5>Password</h5>
-                <input type='password'/>
-
-                <button className='login__signInButton'>
-                    Sign In
+                <button className='login__registerButton' onClick={register} >
+                    Create your Amazon Account
                 </button>
-            </form>
-
-            <p>
-                By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-            </p>
-
-            <button className='login__registerButton'>
-                Create your Amazon Account
-            </button>
+            </div>
+            
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Login
