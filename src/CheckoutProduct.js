da2c4d46440e8265b27097d6ca0a62c28c0b83ae
arpitthool <arpitthool1@gmail.com>
2022-07-04 00:37:25 -0400

    added remove from basket functionality

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import './CheckoutProduct.css'
+import { useStateValue } from './StateProvider'
 
 function CheckoutProduct({ id, image, title, price, rating}) {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        // remove item from bassket
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
 
     return (
         <div className='checkoutProduct'>
@@ -26,7 +37,7 @@ function CheckoutProduct({ id, image, title, price, rating}) {
                     }
                 </div>
 
-                <button>
+                <button onClick={removeFromBasket}>
                     Remove from Basket
                 </button>
             </div>
--- src/reducer.js
+++ src/reducer.js
@@ -13,13 +13,35 @@ const reducer = (state, action) => {
 
     switch(action.type) {
         case "ADD_TO_BASKET":
-        return {
-            ...state,
-            basket: [...state.basket, action.item],
-        };
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
+
+        case "REMOVE_FROM_BASKET":
+            // get the index of the first item that matches passed id and remove it.
+            
+            const index = state.basket.findIndex(
+                (basketIten) => basketIten.id == action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as it's not in basket!`
+                )
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
 
         default:
-        return state;
+            return state;
     }
 };
 
