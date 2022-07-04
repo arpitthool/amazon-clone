53a056f5c6cfce976593cb46dd58f85ef011a3f8
arpitthool <arpitthool1@gmail.com>
2022-07-04 02:16:23 -0400

    created and exported the db and auth variables needed for login functionallity

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyD80cEhSggfTDLF9GT51VYhKrwfS5Apf94",
@@ -7,4 +9,12 @@ const firebaseConfig = {
     messagingSenderId: "998508387464",
     appId: "1:998508387464:web:dd7f54256582c3dfcb533d",
     measurementId: "G-FXFGYWWXP8"
-  };
+};
+
+const firebase = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth};
