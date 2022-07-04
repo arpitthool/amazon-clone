e88d75b8ba316a88616c26758562ceee184e2b5c
arpitthool <arpitthool1@gmail.com>
2022-07-04 03:12:13 -0400

    commented firebase code as the current react version does not meet firebase module dependency

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD80cEhSggfTDLF9GT51VYhKrwfS5Apf94",
-    authDomain: "challenge-3742b.firebaseapp.com",
-    projectId: "challenge-3742b",
-    storageBucket: "challenge-3742b.appspot.com",
-    messagingSenderId: "998508387464",
-    appId: "1:998508387464:web:dd7f54256582c3dfcb533d",
-    measurementId: "G-FXFGYWWXP8"
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD80cEhSggfTDLF9GT51VYhKrwfS5Apf94",
+//     authDomain: "challenge-3742b.firebaseapp.com",
+//     projectId: "challenge-3742b",
+//     storageBucket: "challenge-3742b.appspot.com",
+//     messagingSenderId: "998508387464",
+//     appId: "1:998508387464:web:dd7f54256582c3dfcb533d",
+//     measurementId: "G-FXFGYWWXP8"
+// };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore();
+// const db = firebaseApp.firestore();
 
-const auth = firebaseApp.auth();
+// const auth = firebaseApp.auth();
 
-export { db, auth};
+// export { db, auth};
