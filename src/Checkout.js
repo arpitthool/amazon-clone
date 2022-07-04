96edfea25b091963529b4e864d92d2b9dff67510
arpitthool <arpitthool1@gmail.com>
2022-07-03 23:57:47 -0400

    iterating over basket to show added items

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct'
 import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider'
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
 
     return (
         <div className='checkout'>
@@ -14,10 +18,17 @@ function Checkout() {
                         Your shopping basket
                     </h2>
 
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
+                    {basket.map(
+                        item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        )
+                    )}
                 </div>
             </div>
 
