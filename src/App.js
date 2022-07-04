8fd2bc66f0e2394d1d5a43bdccfcbb2d40b3d840
arpitthool <arpitthool1@gmail.com>
2022-07-04 03:09:16 -0400

    trying to copy the main github repo config

--- src/App.js
+++ src/App.js
@@ -3,25 +3,46 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
-import { BrowserRouter as Router, Routes, Route, Switch} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './Login';
 
 function App() {
-  return (
-    // BEM convention
-    <Router>
+  // return (
+  //   // BEM convention
+  //   <Router>
 
-      <div className="app">
-        <Routes>
+  //     <div className="app">
+  //       <Routes>
 
-          <Route exact path='/' element={<><Header/><Home/></>}/>
-          <Route exact path='/login' element={<><Login/></>}/>
-          <Route exact path='/checkout' element={<><Header/><Checkout/></>} />
+  //         <Route exact path='/' element={<><Header/><Home/></>}/>
+  //         <Route exact path='/login' element={<><Login/></>}/>
+  //         <Route exact path='/checkout' element={<><Header/><Checkout/></>} />
 
-        </Routes>
-      </div>
-    </Router>
+  //       </Routes>
+  //     </div>
+  //   </Router>
     
+  // );
+  // return (
+  //   <Router>
+  //     <div className="app">
+  //       <Header />
+  //       <Switch>
+  //         <Route path="/checkout">
+  //           <Checkout />
+  //         </Route>
+  //         <Route path="/">
+  //           <Home />
+  //         </Route>
+  //       </Switch>
+  //     </div>
+  //   </Router>
+  // );
+  return (
+      <div className="app">
+        <h1>Hello</h1>
+      </div>
   );
 }
 
