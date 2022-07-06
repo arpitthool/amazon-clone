1960be851817b17447881d74958ee123ca960180
arpitthool <arpitthool1@gmail.com>
2022-07-06 19:09:44 -0400

    styled the payment page and added link back to checkout page

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,78 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
+import { useStateValue } from './StateProvider'
 
 function Payment() {
-  return (
-    <div className='payment'>
-        Payment
-    </div>
-  )
+
+    const [{basket, user}, dispatch] = useStateValue();
+
+    return (
+        <div className='payment'>
+
+            <div className='payment__container'>
+
+                <h1>
+                    Checkout (
+                        <Link to='/checkout'>
+                            {basket?.length} items
+                        </Link>
+                    )
+                </h1>
+
+                {/* address */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>
+                            Delivery Address
+                        </h3>
+                    </div>
+                    <div className='payment_-address'>
+                        <p>{user?.email}</p>
+                        <p>123 texas hill dr</p>
+                        <p>Drighstown, GA</p>
+                    </div>
+                </div>
+
+                {/* review cart items */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                            <h3>
+                                Review items and delivery
+                            </h3>
+                    </div>
+                    <div className='payment__items'>
+                        {basket.map(
+                            item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            )
+                        )}
+                    </div>
+                </div>
+
+                {/* payment method */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>
+                            Payment Method
+                        </h3>
+                    </div>
+                    <div className='payment__details'>
+                        {/* Strip payment */}
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Payment
