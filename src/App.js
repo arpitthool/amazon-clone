6c16b2aededf124ae3669cd419ff2e07ee5c69fa
arpitthool <arpitthool1@gmail.com>
2022-07-06 21:22:43 -0400

    created form to make payment, used react-stripe-js library

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
 import Payment from './Payment';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe('pk_test_51LIhTyHuvekNpR8Z1KBRGy1ojzItitbRp7Wcjhw13FJwCjXwafCrDFUsxrOXjnvY1RyH5S8DOnOWi6SyxE5Btj1R00l9Bi17eA');
 
 function App() {
 
@@ -62,7 +66,9 @@ function App() {
 
         <Route exact path="/payment">
           <Header />
-          <Payment />
+          <Elements stripe={promise} >
+            <Payment />
+          </Elements>
         </Route>
       </Switch>
     </div>
--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider'
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import { getBasketTotal } from './reducer';
 
 function Payment() {
 
     const [{basket, user}, dispatch] = useStateValue();
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const [processing, setProcessing] = useState("");
+    const [succeeded, setSucceeded] = useState(false);
+
+    const handleSubmit = e => {
+
+    }
+
+    const handleChange = event => {
+        // listen for changes in card elements and show errors if any
+        
+        // if event is empty then disable the button
+        setDisabled(event.empty);
+
+        // if there is error then show it
+        setError(event.error ? event.error.message : "");
+    }
 
     return (
         <div className='payment'>
@@ -66,6 +92,24 @@ function Payment() {
                     </div>
                     <div className='payment__details'>
                         {/* Strip payment */}
+
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange}/>
+
+                            <div className='payment__priceContainer'>
+                                <h3>
+                                    Order Total : ${getBasketTotal(basket)}
+                                </h3>
+
+                                <button disabled={ processing || disabled || succeeded}>
+                                    <span>
+                                        {processing ? "Processing" : "But Now"}
+                                    </span>
+                                </button>
+                            </div>
+
+                            {error && <div>{error}</div>}
+                        </form>
                     </div>
 
                 </div>
