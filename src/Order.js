d1ff756543ecaeab994f34b73deb78eaab6ac334
arpitthool <arpitthool1@gmail.com>
2022-07-11 01:37:29 -0400

    fetching orders from db and mapping each to an order component

---
+++ src/Order.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Order.css'
+
+function Order({order}) {
+  return (
+    <div className='order'>
+        <h2>
+            Your order!
+        </h2>
+    </div>
+  )
+}
+
+export default Order
