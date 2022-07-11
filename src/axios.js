27505664105a198bf907c273c5a516fc4bf6c064
arpitthool <arpitthool1@gmail.com>
2022-07-11 03:22:31 -0400

    edited cloud fnctions url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // API (cloud function) URL
-    baseURL : 'http://localhost:5001/challenge-3742b/us-central1/api',
+    // baseURL : 'http://localhost:5001/challenge-3742b/us-central1/api',
+    baseURL : 'https://us-central1-challenge-3742b.cloudfunctions.net/api',
 });
 
 export default instance;
